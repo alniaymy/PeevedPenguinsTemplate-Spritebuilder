f71e0d79d40af4b0eb41dbcfcba0a02f13ad0ddf
Nabeel Salih <salih.nabeel@gmail.com>
2014-10-22 23:27:58 -0600

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -13,6 +13,7 @@
     CCPhysicsNode *_physicsNode;
     CCNode *_catapultArm;
     CCNode *_levelNode;
+    CCNode *_contentNode;
 }
 
 // is called when CCB file has completed loading
@@ -44,7 +45,7 @@
     // ensure followed object is in visible area when starting
     self.position = ccp(0, 0);
     CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
-    [self runAction:follow];
+    [_contentNode runAction:follow];
 }
 
 - (void)retry {
