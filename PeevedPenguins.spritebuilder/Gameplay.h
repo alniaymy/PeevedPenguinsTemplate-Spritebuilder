ea2a4718a34a2d9145a0191e25a2990daec00028
Nabeel Salih <salih.nabeel@gmail.com>
2014-10-22 22:36:35 -0600

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Nabeel Salih on 2014-10-22.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
