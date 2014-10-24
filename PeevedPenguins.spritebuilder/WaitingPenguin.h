cca64c7283fcbea2c94e08a35859d2ce75c23715
Nabeel Salih <salih.nabeel@gmail.com>
2014-10-23 23:47:25 -0600

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by Nabeel Salih on 2014-10-23.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end
